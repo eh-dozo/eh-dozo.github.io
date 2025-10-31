6a4d0a33827a9a54db8c269d7ec76d6cfc77ac00
eliot.hoff <eliot.hoff@sweap.io>
2025-10-31 14:41:00 +0100

    - inset shadow

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -13,6 +13,9 @@
 </script>
 
 <header class="flex w-full justify-center py-[7lvh]">
+	<div
+		class="absolute h-[19lvh] w-[7lvw] -translate-x-0.5 rounded-lg inset-shadow-xs inset-shadow-gray-200"
+	></div>
 	<button
 		type="button"
 		class="-rotate-90 text-[7lvw] font-extrabold tracking-[-0.08em] text-white mix-blend-difference {animating
