df8e8bb7d016f68117d2f414d633abb18b5bd4d3
eliot.hoff <eliot.hoff@sweap.io>
2025-11-01 18:42:45 +0100

    - add new images - remove unused images

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,7 +25,7 @@
 </svelte:head>
 
 <enhanced:img
-	src="/src/lib/assets/bg.jpg"
+	src="/src/lib/assets/bg-gogo.avif"
 	alt=""
 	aria-hidden="true"
 	class="pointer-events-none fixed inset-0 -z-10 size-full scale-(--html-bg-size) overflow-hidden object-cover blur-[5px]"
